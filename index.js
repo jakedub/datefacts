2e27d3d7bc16e56b2552a1684b1a4e8d2dc31f63
jakedub <jacob.w.moore@gmail.com>
2017-08-14 14:30:29 -0400

    one two and three done, problem with date

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const chalk = require('chalk');
-var moment = require('moment');
+let moment = require('moment');
 moment().format();
 
 
-let current_time = new moment().format("LLLL");
-let beginning = moment("January 1, 2017").format("LLLL");
-let difference = current_time - beginning;
+let current_time = new moment().format("LLLL"); //first
+let now = moment(new Date()); //second
+let end = moment("2017-1-1");
+let duration = moment.duration(now.diff(end));
+let days = Math.round(duration.asDays());
+
+let secondStart = moment ("2017-14-08");
+let secondDuration = moment.duration(now.diff(end));
+let secondTotal = Math.round(duration.asSeconds());
 
 
 console.log(`It is ${current_time}.`);
-console.log(`It is the ${difference} day of the year`); //number is the count of days
-console.log(`It is number seconds into the day`);//number is the count of seconds in today
+console.log(`It is the ${days}th day of the year`); //something broken but correct
+console.log(`It is ${secondTotal} seconds into the day`);//number is the count of seconds in today
 console.log(`It is during Daylight Savings Time`);//assuming is in reference to whether it's DSL
 console.log(`It is not a leap year`);//is not is checking if it's currently a leap year
